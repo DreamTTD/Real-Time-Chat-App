e7993afe1a9e66fa99f334c38cae387a85e96b60
Dream <87536032+Dream1027@users.noreply.github.com>
2026-04-30 17:10:36 +0200

    Revert "update"
    This reverts commit 63dbfdef3e72b5f69077da32aa209f1184f20f40.

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-
-const io = require('socket.io')(3001, { cors: { origin: '*' } });
-
-io.on('connection', (socket) => {
-  socket.on('sendMessage', (msg) => {
-    socket.broadcast.emit('receiveMessage', msg);
-  });
-});
